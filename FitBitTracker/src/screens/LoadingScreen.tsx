6936aff0be44ff668e4d8e4d7b528de385c3ed35
rahul narayan <rahulnarayan@rahuls-MacBook-Pro.local>
2020-12-29 16:28:53 +0100

    completed code to retrieve steps and floors climbed

--- FitBitTracker/src/screens/LoadingScreen.tsx
+++ FitBitTracker/src/screens/LoadingScreen.tsx
@@ -5,17 +5,25 @@ import { Text, View, ScrollView, StyleSheet, ActivityIndicator, TouchableOpacity
 // Services
 import { FitBitService } from '../services/fitBitService';
 
+// Components
+import Steps from '../components/Steps';
+import Floors from '../components/Floors';
+
 export default class LoadingScreen extends Component<{ navigation: any }>{
     componentDidMount = async () => {
         const fb = new FitBitService();
         await fb.authorizeFitBit();
-        await fb.fetchActivityInRange("steps", new Date("2020-12-01"), new Date("2020-12-01"));
     }
 
     render(){
         return(
             <View>
-                <Text>main fitbit tracker screen</Text>
+                <View>
+                    <Steps />
+                </View>
+                <View>
+                    <Floors />
+                </View>
             </View>
         )
     }
